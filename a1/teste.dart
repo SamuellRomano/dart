7c71117cdf03c33a041baf65affa90c939d02f14
Samuel Romano <105526234+SamuellRomano@users.noreply.github.com>
2024-02-28 08:02:54 -0300

    Update teste.dart

--- a1/teste.dart
+++ a1/teste.dart
@@ -1,19 +1,21 @@
 void main() {
+  //ex1
   var num = 10;
 
-  if (num % 2 == 0) {
-    print('O número $num é par.');
+  if (num > 0) {
+    print('O número $num é positivo.');
   } else {
-    print('O número $num é impar.');
+    print('O número $num é negativo (ou nulo).');
   }
+//ex2
 
   var num2 = 20;
-
   var mensagem =
       num2 % 2 == 0 ? ' número $num é par.' : 'O número $num é impar';
 
   print(mensagem);
 
+  //ex3
   int a = 20;
   if (a % 5 == 0) {
     print('O número $a ' 'é multiplo de 5');
